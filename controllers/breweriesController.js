6ed9241ddcb994aaa02dba18150c283166867515
jacobjohn90 <jacobjohn90@gmail.com>
2018-06-07 20:11:54 -0400

    created new router and page for brewery

--- controllers/breweriesController.js
+++ controllers/breweriesController.js
@@ -1,22 +1,44 @@
 const express = require('express')
-const router = express.Router({mergeParams: true})
+const router = express.Router({ mergeParams: true })
 
 const User = require('../models/User')
 
 //GET TO BREWERY INDEX
-router.get('/', (req, res)=> {
+router.get('/', (req, res) => {
     const userId = req.params.userId
 
     User.findById(userId)
-        .then((user)=>{
+        .then((user) => {
             res.render('brewery/index', {
                 breweries: user.brewCheck,
                 user,
             })
         })
-        .catch((err)=> {
+        .catch((err) => {
             console.log('Error getting to index page of Breweries. Error is: ' + err)
         })
 })
+
+//NEW ROUTE FOR NEW BREWERY
+router.get('/new', (req, res) => {
+    const userId = req.params.userId
+
+    res.render('brewery/new', {
+        userId
+    })
+        .catch((err) => {
+            console.log('Error trying to go to create a New Brewery! The error is: ' + err)
+        })
+})
+//CREATE ROUTE FOR NEW BREWERY
+
+//SHOW ROUTE FOR SPECIFIC BREWERY
+
+//EDIT ROUTE FOR SPECIFIC BREWERY
+
+//UPDATE ROUTE FOR SPECIFIC BREWERY
+
+//DELETE ROUTE FOR SPECIFIC BREWERY
+
 
 module.exports = router
