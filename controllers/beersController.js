9fb009c137e76588edd4870733f65d4fb9f35bec
jacobjohn90 <jacobjohn90@gmail.com>
2018-06-08 16:37:03 -0400

    post button to add new Beer works. No Errors

--- controllers/beersController.js
+++ controllers/beersController.js
@@ -26,23 +26,33 @@ router.get('/', (req, res)=> {
 })
 //NEW ROUTE TO ADD NEW BEER
 router.get('/new', (req, res)=> {
+    res.render('beer/new', {
+        userId: req.params.userId,
+        breweryId: req.params.breweryId
+    })
+
+})
+//CREATE ROUTE
+router.post('/', (req, res)=> {
     const userId = req.params.userId
     const breweryId = req.params.breweryId
+    const newBeer = new Beer(req.body)
 
     User
         .findById(userId)
         .then((user)=> {
-            const brewery = user.brewCheck.id(breweryId)
 
-            res.render('beer/new'), {
-                userId,
-                breweryId,
-                brewery
-            }
+            const brewery = user.brewCheck.id(breweryId)
+            brewery.popularBeers.push(newBeer) 
+            return user.save()           
+        })
+        .then(()=> {
+            res.redirect(`/users/${userId}/breweries/${breweryId}/beers`)
+        })
+        .catch((err)=> {
+            console.log('Error POSTING new beer and redirecting to beers index. Error is: '+ err)
         })
 })
-//CREATE ROUTE
-
 //SHOW ROUTE TO SHOW SPECIFIC BEER
 
 //EDIT ROUTE TO EDIT SPECIFIC BEER
