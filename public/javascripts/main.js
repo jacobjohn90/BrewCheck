1f297893cbaa9ccfa44dd1b312e47ae1480fcac9
jacobjohn90 <jacobjohn90@gmail.com>
2018-06-12 10:01:36 -0400

    added more Info Button with bootbox

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,1 +1,12 @@
 console.log('hello world')
+window.alert = function(message){
+    bootbox.alert(message)
+}
+
+$('#moreInfoBtn').click(function () {
+    alert({ 
+        size: "small",
+        title: "More Info on BrewBox",
+        message: "BrewCheck is a simple app that allows you to make a checklist of all the breweries you've been too as well as breweries you would like to go to. Go ahead and make a new user, add some breweries and its information, and let others know what beers they have and rate them!",
+    })
+})
