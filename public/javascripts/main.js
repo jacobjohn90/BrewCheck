30227a4e36dc35091fa4aa3904ff87c9548817b2
jacobjohn90 <jacobjohn90@gmail.com>
2018-06-13 08:49:39 -0400

    spelling error

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -6,6 +6,6 @@ $('#moreInfoBtn').click(function () {
     alert({ 
         size: "small",
         title: "More Info on BrewBox",
-        message: "BrewCheck is a simple app that allows you to make a checklist of all the breweries you've been too as well as breweries you would like to go to. Go ahead and make a new user, add some breweries and its information, and let others know what beers they have and rate them!",
+        message: "BrewCheck is a simple app that allows you to make a checklist of all the breweries you've been to as well as breweries you would like to go to. Go ahead and make a new user, add some breweries and its information, and let others know what beers they have and rate them!",
     })
 })
